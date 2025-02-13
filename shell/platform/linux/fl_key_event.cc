fe45c13dbec0f9bb7b8df0b7c2ff0c3addc91312
Mikolaj Staworzynski <mikolaj.staworzynski@redembedded.com>
2025-02-13 13:08:48 +0100

    Revert "Add missing cstring header"
    This reverts commit e18e9a085c8fe4a79138910292c812fbbddd9f34.

--- shell/platform/linux/fl_key_event.cc
+++ shell/platform/linux/fl_key_event.cc
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <cstring>
 #include "flutter/shell/platform/linux/fl_key_event.h"
 
 struct _FlKeyEvent {
